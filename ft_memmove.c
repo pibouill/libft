c6cea7a2adfc6876c6c7d7170571d623ba2c7ba8
pibouill <pibouill@student.42prague.com>
2023-10-18 10:31:27 +0200

    kfopds

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: pibouill <pibouill@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 17:14:34 by pibouill          #+#    #+#             */
-/*   Updated: 2023/10/17 17:47:40 by pibouill         ###   ########.fr       */
+/*   Updated: 2023/10/17 18:14:45 by pibouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		dest_str[i] = src_str[i];
 		i++;
 	}
-
+	dest_str[i] = '\0';
+	return (dest_str);
 }
