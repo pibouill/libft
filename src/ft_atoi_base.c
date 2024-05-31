a679ef83169e8da667a306da8f0645228832c85d
pibouill <pibouill@student.42prague.com>
2024-05-31 15:47:12 +0200

    atoi_base update hex prefix

--- src/ft_atoi_base.c
+++ src/ft_atoi_base.c
@@ -6,7 +6,7 @@
 /*   By: pibouill <pibouill@student.42prague.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 17:40:26 by pibouill          #+#    #+#             */
-/*   Updated: 2024/04/21 14:19:14 by pibouill         ###   ########.fr       */
+/*   Updated: 2024/05/31 15:46:46 by pibouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 static int	is_valid_char(char c)
 {
 	return ((c >= 'a' && c <= 'f')
-		|| (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
+		|| (c >= 'A' && c <= 'F') || (c >= '0' && c <= '9'));
+}
+
+static int	is_hex_prefix(const char *prefix)
+{
+	return (*prefix == '0' && *(prefix + 1) == 'x');
 }
 
 int	ft_atoi_base(const char *str, unsigned int base)
@@ -31,6 +36,8 @@ int	ft_atoi_base(const char *str, unsigned int base)
 			sign *= -1;
 		str++;
 	}
+	if (is_hex_prefix(str) == 1)
+		str += 2;
 	while (*str && is_valid_char(*str))
 	{
 		if (*str >= '0' && *str <= '9')
@@ -43,3 +50,11 @@ int	ft_atoi_base(const char *str, unsigned int base)
 	}
 	return (ret * sign);
 }
+
+/*int	main()
+{
+	printf("%d\n", ft_atoi_base("0x810202", 16));
+	printf("%d\n", ft_atoi_base("0xFFFFFF", 16));
+	printf("%d\n", ft_atoi_base("10", 2));
+	printf("%d\n", ft_atoi_base("42f", 16));
+}*/
