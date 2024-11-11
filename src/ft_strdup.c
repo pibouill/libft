c04dfd576d9b1b1b3265facfaa5ae1a086af554e
pibouill <pibouill@student.42prague.com>
2024-11-11 17:02:53 +0100

    fixing strdup() leaks??

--- src/ft_strdup.c
+++ src/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: pibouill <pibouill@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 16:22:09 by pibouill          #+#    #+#             */
-/*   Updated: 2024/10/10 21:16:05 by pibouill         ###   ########.fr       */
+/*   Updated: 2024/11/11 16:58:10 by pibouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,43 @@
 ** Duplicates *s
 */
 
-char	*ft_strdup(const char *s)
+//char	*ft_strdup(const char *s)
+//{
+//    char	*new;
+//    int		i;
+
+//    i = 0;
+//    new = malloc(sizeof(char) * ft_strlen(s) + 1);
+//    if (new == NULL)
+//        return (NULL);
+//    while (*s)
+//        new[i++] = *s++;
+//    new[i] = '\0';
+//    return (new);
+//}
+
+char	*ft_strdup(const char *src)
 {
-	char	*new;
+	char	*str;
 	int		i;
 
-	i = 0;
-	new = malloc(sizeof(char) * ft_strlen(s) + 1);
-	if (new == NULL)
+	if (!src)
 		return (NULL);
-	while (*s)
-		new[i++] = *s++;
-	new[i] = '\0';
-	return (new);
+	i = 0;
+	if (src[0] == '\0')
+		str = malloc(1);
+	else
+	{
+		while (src[i])
+			i++;
+		str = malloc((i + 1) * sizeof(char));
+		i = 0;
+		while (src[i])
+		{
+			str[i] = src[i];
+			i++;
+		}
+	}
+	str[i] = '\0';
+	return (str);
 }
